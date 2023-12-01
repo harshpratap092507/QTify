604956071627208e8660811411d0a8b9fdb184db
harshpratap092507 <78509544+harshpratap092507@users.noreply.github.com>
2023-12-01 15:00:56 +0530

    button type

--- src/components/Button/Button.jsx
+++ src/components/Button/Button.jsx
@@ -2,7 +2,11 @@ import React from "react";
 import styles from "./button.module.css";
 
 const Button = ({ children }) => {
-  return <button className={styles.Feedback_container}>{children}</button>;
+  return (
+    <button type="submit" className={styles.Feedback_container}>
+      {children}
+    </button>
+  );
 };
 
 export default Button;
